bc58b6dce1a11b1eb628e6b384c3dff0beae0b9c
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-11-17 09:20:49 -0600

    add generate_label function

--- src/pipelines/ingestion.py
+++ src/pipelines/ingestion.py
@@ -9,8 +9,10 @@ def drop_cols(df):
     pass
 
 
-def generate_label():
-    pass
+def generate_label(df):
+    df['label'] = df['codigo_cierre'].str.split(' ', n=1, expand=False)
+    df['label'] = df['label'].apply(lambda x: x[0][1])
+    df['label'] = df['label'].apply(lambda x: 1 if x == 'F' or x == 'N' else 0)
 
 
 def save_ingestion(df):
