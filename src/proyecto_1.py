21aad65d78c03978fccbc582d2424d56c4d2c681
Angel Rafael Ortega Ramírez <51694410+rafaelortegar@users.noreply.github.com>
2020-11-25 23:04:50 -0600

    update final proyecto_1

--- src/proyecto_1.py
+++ src/proyecto_1.py
@@ -9,14 +9,14 @@ from src.pipelines.feature_engineering import feature_engineering
 # paths
 csv_path = '../data/incidentes-viales-c5.csv'
 ingestion_train_pickle_path = '../output/ingest_df.pkl'
-ingestion_test_pickle_path = '../output/ingest_test_df.pkl'
+test_df_pickle_path = '../output/test_df.pkl'
 transformation_pickle_path = '../output/transformation_df.pkl'
 feature_engineering_pickle_path = '../output/fe_df.pkl'
 
 
 def main():
-    ingest(csv_path, ingestion_train_pickle_path,ingestion_test_pickle_path)
-    transform(ingestion_train_pickle_path, transformation_pickle_path)
+    ingest(csv_path, ingestion_train_pickle_path)
+    transform(ingestion_train_pickle_path, transformation_pickle_path,test_df_pickle_path)
     feature_engineering(transformation_pickle_path,feature_engineering_pickle_path)
 
 if __name__ == '__main__':
